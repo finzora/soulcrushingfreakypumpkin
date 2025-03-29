10b3096a8e29a15fbf9c0b54123c3f70d89c1093
finzora <finncolonthree@gmail.com>
2025-03-28 23:15:58 -0400

    Update towers.js

--- json/towers.js
+++ json/towers.js
@@ -1,6 +1,10 @@
 all_towers = [
   {"id": 1, "abbr": "ToP777", "name": "Tower of Peacebringer 7 7 7", "diff": 860, "places": [], "game": "https://www.roblox.com/games/109627705101665/Tower-of-Peace-Bringer-Seven-Seven"},
   {"id": 2, "abbr": "ANT", "name": "Arabian Nights Tower", "diff": 1478, "places": [], "game": "https://www.roblox.com/games/15336594824/Arabian-Night-Tower"},
-  {"id": 3, "abbr": "TfSH", "name": "Tower for Super Heroes", "diff": 1400, "places": [], "game": "https://www.roblox.com/games/16824392474/unnamed"},
-  {"id": 4, "abbr": "ToH", "name": "Tower of Hell", "diff": 201, "places": [], "game": "https://www.roblox.com/games/1962086868/Tower-of-Hell"}
+  {"id": 3, "abbr": "TfSH", "name": "Tower for Super Heroes", "diff": 1407, "places": [], "game": "https://www.roblox.com/games/16824392474/unnamed"},
+  {"id": 4, "abbr": "ToH", "name": "Tower of Hell", "diff": 201, "places": [], "game": "https://www.roblox.com/games/1962086868/Tower-of-Hell"},
+  {"id": 5, "abbr": "HT", "name": "Hell Tower", "diff": 1400, "places": [], "game": "https://www.roblox.com/games/16471863080/unnamed"},
+  {"id": 6, "abbr": "Esfjc", "name": "Every sc final jump combined", "diff": 795.5, "places": [], "game": "https://www.roblox.com/games/8887862989/Every-sc-final-jump-combined"},
+  {"id": 7, "abbr": "깨월좀하타", "name": "깨면 월홉 좀 하는 타워면 월홉 좀 하는 타워", "diff": 1494, "places": [], "game": "https://www.roblox.com/games/79059018631692/unnamed"},
+  {"id": 8, "abbr": "NDS", "name": "Natural Disaster Survival", "diff": 133, "places": [], "game": "https://www.roblox.com/games/189707/Natural-Disaster-Survival"}
 ]
