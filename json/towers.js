a9809663c61fe42d55e1a87d6bd2a1cde85a14c4
finzora <finncolonthree@gmail.com>
2025-03-28 22:30:05 -0400

    Update towers.js

--- json/towers.js
+++ json/towers.js
@@ -1,3 +1,4 @@
 all_towers = [
   {"id": 1, "abbr": "ToP777", "name": "Tower of Peacebringer 7 7 7", "diff": 860, "places": [], "game": "https://www.roblox.com/games/109627705101665/Tower-of-Peace-Bringer-Seven-Seven"}
+  {"id": 2, "abbr": "ANT", "name": "Arabian Nights Tower", "diff": 1478, "places": [], "game": "https://www.roblox.com/games/15336594824/Arabian-Night-Tower"}
 ]
