717b03273e6c050a290c333db6e132e95648ceda
finzora <finncolonthree@gmail.com>
2025-03-29 01:39:28 -0400

    Update comps.js

--- json/comps.js
+++ json/comps.js
@@ -1,7 +1,7 @@
 var all_completions = [
   {"name":"thej10n","completions":[1, 7, 8, 4, 6]},
-  {"name":"zFinzora","completions":[1]},
-  {"name":"doubleyoukayy","completions":[8, 4, 10]},
-  {"name":"thecoolestboyevardud","completions":[1, 8, 4, 6, 11, 10, 7]},
+  {"name":"zFinzora","completions":[1, 13, 12, 4, 8]},
+  {"name":"doubleyoukayy","completions":[8, 4, 10, 14]},
+  {"name":"thecoolestboyevardud","completions":[1, 8, 4, 6, 11, 10, 7, 12]},
   {"name":"mertmisinsen0","completions":[8, 4, 6, 9, 10]}
   ]
