955d1eb48f86c25ba5e61a3ce6ca9ba27c92c9b3
finzora <finncolonthree@gmail.com>
2025-03-29 01:27:02 -0400

    Update towers.js

--- json/towers.js
+++ json/towers.js
@@ -1,6 +1,6 @@
 all_towers = [
   {"id": 1, "abbr": "ToP777", "name": "Tower of Peacebringer 7 7 7", "diff": 860, "places": [], "game": "https://www.roblox.com/games/109627705101665/Tower-of-Peace-Bringer-Seven-Seven"},
-  {"id": 2, "abbr": "ANT", "name": "Arabian Nights Tower", "diff": 1478, "places": [], "game": "https://www.roblox.com/games/15336594824/Arabian-Night-Tower"},
+  {"id": 2, "abbr": "ANT", "name": "Arabian Nights Tower", "diff": 1487, "places": [], "game": "https://www.roblox.com/games/15336594824/Arabian-Night-Tower"},
   {"id": 3, "abbr": "TfSH", "name": "Tower for Super Heroes", "diff": 1407, "places": [], "game": "https://www.roblox.com/games/16824392474/unnamed"},
   {"id": 4, "abbr": "ToH", "name": "Tower of Hell", "diff": 201, "places": [], "game": "https://www.roblox.com/games/1962086868/Tower-of-Hell"},
   {"id": 5, "abbr": "HT", "name": "Hell Tower", "diff": 1400, "places": [], "game": "https://www.roblox.com/games/16471863080/unnamed"},
@@ -12,5 +12,6 @@ all_towers = [
   {"id": 11, "abbr": "SO", "name": "Sprunki Obby", "diff": 1300.53, "places": [], "game": "https://www.roblox.com/games/123356501098961/SHOP-UPDATE-Sprunki-Obby"},
   {"id": 12, "abbr": "bd:RAG", "name": "better discovery: Review Any Game", "diff": 1135, "places": [], "game": "https://www.roblox.com/games/15317947079/better-discovery"},
   {"id": 13, "abbr": "TI", "name": "Tiny Isles", "diff": 1308, "places": [["Adopt Me!", "Obbies"]]},
-  {"id": 14, "abbr": "TG", "name": "Touch Grass", "diff": 1100, "places": []}
+  {"id": 14, "abbr": "TG", "name": "Touch Grass", "diff": 1100, "places": []},
+  {"id": 15, "abbr": "TTTT", "name": "Tung Tung Tung Tower", "diff": 1178, "places": [], "game": "https://www.roblox.com/games/70503660100467/Moon-Tung-Tung-Tung-Tower"}
 ]
