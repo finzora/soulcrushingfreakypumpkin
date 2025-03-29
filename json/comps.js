be5d951b501b9d05a812c77f5cbe78ccda83c93d
finzora <finncolonthree@gmail.com>
2025-03-29 00:08:53 -0400

    Update comps.js

--- json/comps.js
+++ json/comps.js
@@ -1,4 +1,6 @@
 var all_completions = [
   {"name":"thej10n","completions":[1, 7]},
-  {"name":"zFinzora","completions":[1]}
+  {"name":"zFinzora","completions":[1]},
+  {"name":"doubleyoukayy","completions":[8, 4]},
+  {"name":"thecoolestboyevardud","completions":[1, 8, 4]}
   ]
